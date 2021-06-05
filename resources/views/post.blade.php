de6dd9f582fdda730d6134b73ce1b513c2773f1a
Kerim <darckqueen144@gmail.com>
2021-06-05 12:37:01 +0300

    Routing for posting

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>All posts</h1>
+    @foreach ($posts as $p)
+        <h3>{{$p->Name}}</h3>
+        <h3>{{$p->Surname}}</h3>
+        <h3>{{$p->Department}}</h3>
+    @endforeach
+</body>
+</html>
