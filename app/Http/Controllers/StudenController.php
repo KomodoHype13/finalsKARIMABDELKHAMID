e0d740af58a1670add37b5b91204c3af4ffc0237
Kerim <darckqueen144@gmail.com>
2021-06-05 12:29:00 +0300

    controller

---
+++ app/Http/Controllers/StudenController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class StudenController extends Controller
+{
+
+    public function getAllPosts(){
+        
+        $posts = DB::table('final_examkarims')->get();
+        return view ('posts', compact('posts'));
+
+    }
+    
+}
