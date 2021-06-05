1773c588fccc9d10fd9514b81ef75612de7f9e04
Kerim <darckqueen144@gmail.com>
2021-06-05 12:43:56 +0300

    question 11

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>KARIM ABDEL-KHAMID</title>
 </head>
 <body>
     <h1>All posts</h1>
